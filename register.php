1348f92552d55695f0080edffb9d4326578058e8
yiying10 <suelle1114@gmail.com>
2024-11-19 16:06:56 +0800

    renamed:    Pekemon.sql -> Pokemon.sql 	deleted:    cards.html 	new file:   cards.php 	renamed:    generate.html -> generate.php 	modified:   home.html 	new file:   home.php 	modified:   login.php 	new file:   logout.php 	deleted:    reference.html 	new file:   reference.php 	modified:   register.css 	modified:   register.html 	modified:   register.php 	modified:   styles.css

--- register.php
+++ register.php
@@ -1,19 +1,15 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    echo "表單提交成功！";
-    exit();
-} else {
-    echo "非 POST 請求";
-    exit();
-}
-?>
-<?php
+// 開啟錯誤訊息顯示
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 // 資料庫連線設定
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "Pokemon";
 
+// 建立連接
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // 檢查連線
@@ -23,35 +19,44 @@ if ($conn->connect_error) {
 
 // 確認表單是否提交
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['name'];
-    $username = $_POST['username'];
+    $user_name = $_POST['user_name'];
+    $account = $_POST['account'];
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT); // 密碼加密
 
     // 檢查帳號是否已存在
-    $checkUserQuery = "SELECT * FROM users WHERE username = ?";
+    $checkUserQuery = "SELECT * FROM account WHERE account = ?";
     $stmt = $conn->prepare($checkUserQuery);
-    $stmt->bind_param("s", $username);
-    $stmt->execute();
-    $result = $stmt->get_result();
 
-    if ($result->num_rows > 0) {
-        // 帳號已存在，重新導向並顯示錯誤訊息
-        header("Location: register.html?error=1");
-    } else {
-        // 帳號不存在，插入新資料
-        $insertUserQuery = "INSERT INTO users (name, username, password) VALUES (?, ?, ?)";
-        $stmt = $conn->prepare($insertUserQuery);
-        $stmt->bind_param("sss", $name, $username, $password);
+    if ($stmt) {
+        $stmt->bind_param("s", $account);
+        $stmt->execute();
+        $result = $stmt->get_result();
 
-        if ($stmt->execute()) {
-            // 註冊成功，重新導向並顯示成功訊息
-            header("Location: register.html?success=1");
+        if ($result->num_rows > 0) {
+            // 帳號已存在，顯示錯誤訊息
+            echo "<script>
+                    alert('此帳號已註冊！');
+                    window.history.back();
+                  </script>";
         } else {
-            echo "錯誤：" . $stmt->error;
-        }
-    }
+            // 帳號不存在，插入新資料
+            $insertUserQuery = "INSERT INTO account (user_name, account, password) VALUES (?, ?, ?)";
+            $stmt = $conn->prepare($insertUserQuery);
+            $stmt->bind_param("sss", $user_name, $account, $password);
 
-    $stmt->close();
+            if ($stmt->execute()) {
+                echo "<script>
+                        alert('註冊成功！');
+                        window.location.href = 'home.php';
+                      </script>";
+            } else {
+                echo "錯誤：" . $stmt->error;
+            }
+        }
+        $stmt->close();
+    } else {
+        echo "錯誤：" . $conn->error;
+    }
 }
 
 $conn->close();
