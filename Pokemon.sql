6f23f5cb0a775a8ee7ebf8225173e36691c07103
PUZnutella <youzi9304@gmail.com>
2024-11-27 14:16:11 +0800

    移除不需要的檔案

--- Pokemon.sql
+++
@@ -1,8 +0,0 @@
-USE Pokemon;
-
-CREATE TABLE account (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_name VARCHAR(50) NOT NULL,
-    account VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
-);
